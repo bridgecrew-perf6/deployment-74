84df1caf8362ffdc262673f55bc40d3b0eecd240
sumithra_ram <64328526+Sumithra9900@users.noreply.github.com>
2022-06-20 18:47:29 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -17,8 +17,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    print("Execution Started2")
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
+    print("Execution Started1")
     main()
